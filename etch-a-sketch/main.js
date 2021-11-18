716bc3dca813a34effeac8358d72818705f8a02a
dinesh-prabhu <dineshprabhu.s@outlook.com>
2021-11-18 23:34:15 +0530

    Changed square colors to random.

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -5,18 +5,34 @@ changeGrid.addEventListener("click", getGridSize)
 container.addEventListener("mouseover", gridListener);
 
 function getGridSize() {
-    let size = parseInt(window.prompt("Please enter the new grid size between 1 - 36"));
-    if(size >= 1 && size <= 36) {
+    let size = parseInt(window.prompt("Please enter the new grid size between 1 - 64"));
+    if(size >= 1 && size <= 64) {
         createGrid(size);
     } else {
         getGridSize();
     }
 }
 
+function randomColorGenerator() {
+    let rgb = [];
+    for(let i = 0; i < 3; i++) {
+       rgb.push(Math.floor(Math.random() * 255));
+    };
+    return `rgb(${rgb.toString()})`;
+}
+
 function gridListener(event) {
-    let targetClassList = event.target.classList
-    if(targetClassList.contains("square")) {
-        targetClassList.add("bg-black");
+    if(event.target.classList.contains("square")) {
+        let targetBgColor = event.target.style.backgroundColor;
+        if(targetBgColor.length != 0) {
+            let [r, g, b, a] = targetBgColor.match(/[\d\.]+/g);
+            a = a ?? 1;
+            if(a != 0) {
+                event.target.style.backgroundColor = `rgba(${r}, ${b}, ${g}, ${a - 0.1})`;
+            }
+        } else {
+            event.target.style.backgroundColor = randomColorGenerator();
+        }
     }
 }
 
