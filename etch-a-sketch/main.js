1a8e5dcd9c85bc03c472ed4e56c4100f6327c203
dinesh-prabhu <dineshprabhu.s@outlook.com>
2021-11-20 23:51:47 +0530

    Fixed random color with opacity increase logic.

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,13 +1,16 @@
 const changeGrid = document.querySelector("#change-grid");
+const resetGrid = document.querySelector("#reset-grid");
 const container = document.querySelector("#container");
+let currentGridSize;
 
-changeGrid.addEventListener("click", getGridSize)
+changeGrid.addEventListener("click", getGridSize);
+resetGrid.addEventListener("click", createGrid);
 container.addEventListener("mouseover", gridListener);
 
 function getGridSize() {
     let size = parseInt(window.prompt("Please enter the new grid size between 1 - 64"));
     if(size >= 1 && size <= 64) {
-        createGrid(size);
+        changeGridSize(size);
     } else {
         getGridSize();
     }
@@ -21,6 +24,11 @@ function randomColorGenerator() {
     return `rgb(${rgb.toString()})`;
 }
 
+function changeGridSize(size) {
+    currentGridSize = size;
+    createGrid();
+}
+
 function gridListener(event) {
     if(event.target.classList.contains("square")) {
         let targetBgColor = event.target.style.backgroundColor;
@@ -28,7 +36,7 @@ function gridListener(event) {
             let [r, g, b, a] = targetBgColor.match(/[\d\.]+/g);
             a = a ?? 1;
             if(a != 0) {
-                event.target.style.backgroundColor = `rgba(${r}, ${b}, ${g}, ${a - 0.1})`;
+                event.target.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a - 0.1})`;
             }
         } else {
             event.target.style.backgroundColor = randomColorGenerator();
@@ -36,10 +44,10 @@ function gridListener(event) {
     }
 }
 
-function createGrid(size) {
+function createGrid() {
     container.textContent = "";
-    container.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`)
-    let totalSquares = size ** 2;
+    container.setAttribute("style", `grid-template-columns: repeat(${currentGridSize}, 1fr); grid-template-rows: repeat(${currentGridSize}, 1fr);`);
+    let totalSquares = currentGridSize ** 2;
     for(let i = 0; i < totalSquares; i++) {
         let squareElement = document.createElement("div");
         squareElement.className = "square";
@@ -47,4 +55,4 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
+changeGridSize(16);
